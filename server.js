e41d2e3918855112df2c94add919548bd1e1e7cf
pfysches-boop <141571088+pfysches-boop@users.noreply.github.com>
2026-04-19 23:21:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ app.use(cors({
   optionsSuccessStatus: 200,
 }))
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "React app URL"
+  )
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  res.header("Access-Control-Allow-Headers", "Content-Type")
+  next()
+})
+
 app.use(express.static(__dirname))
 
 const socketio = require('socket.io')
